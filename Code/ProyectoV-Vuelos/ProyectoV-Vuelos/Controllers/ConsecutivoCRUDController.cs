2bbb9f354fe5a0728def0c6994856aadee2be4b0
Karl Aase <karl23aase@gmail.com>
2020-10-29 18:41:34 -0600

    CRUD en Desarrollo

---
+++ Code/ProyectoV-Vuelos/ProyectoV-Vuelos/Controllers/ConsecutivoCRUDController.cs
@@ -0,0 +1,30 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ProyectoV_Vuelos.Controllers
+{
+    public class ConsecutivoCRUDController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            Consecutivos db = new Consecutivos();
+
+            return View();
+        }
+
+
+
+    }
+
+
+
+
+}
