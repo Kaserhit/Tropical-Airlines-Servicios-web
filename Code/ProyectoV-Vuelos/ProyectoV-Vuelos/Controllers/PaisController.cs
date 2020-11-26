6e2f7941835ee562366708df38b0d83abaa6d4d5
dunmergithub <58483137+dunmergithub@users.noreply.github.com>
2020-11-26 09:48:08 -0600

    API de Pais Funcional
    GET, GET(id), POST, PUT y DELETE funcionales del controlador de Pais

--- Code/ProyectoV-Vuelos/ProyectoV-Vuelos/Controllers/PaisController.cs
+++ Code/ProyectoV-Vuelos/ProyectoV-Vuelos/Controllers/PaisController.cs
@@ -5,38 +5,92 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Description;
+using BLL;
 
 namespace ProyectoV_Vuelos.Controllers
 {
     public class PaisController : ApiController
     {
         PaisCRUDController CRUD = new PaisCRUDController();
+        Pais PAIS = new Pais();
 
-        // GET: api/Consecutivos
-        public IEnumerable<PaisModel> Get()
+        // GET: api/Pais
+        public IEnumerable<PaisModel> GetPais()
         {
             return CRUD.BuscarPaises();
         }
 
-        // GET: api/Consecutivos/5
-        public PaisModel Get(int id)
+        // GET: api/Pais/5
+        public IHttpActionResult GetPais(int id)
         {
-            return CRUD.BuscarPaises().Where(e => e.PAISID == id).First();
+            PaisModel p = CRUD.BuscarPaises().Where(e => e.PAISID == id).First();
+
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(p);
         }
 
-        // POST: api/Consecutivos
-        public void Post([FromBody] string value)
+        // POST: api/Pais
+        [ResponseType(typeof(PaisModel))]
+        public IHttpActionResult PostPais(PaisModel p)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            PAIS.Generar(p.Consec_Pais, p.CodPais, p.Nombre, p.Imagen);
+
+            return CreatedAtRoute("DefaultApi", new { id = p.PAISID }, p);
         }
 
-        // PUT: api/Consecutivos/5
-        public void Put(int id, [FromBody] string value)
+        // PUT: api/Pais/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPais(int id, PaisModel p)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != p.PAISID)
+            {
+                return BadRequest();
+            }
+
+            if (!PaisExists(id))
+            {
+                return NotFound();
+            }
+
+            PAIS.Actualizar(p.PAISID, p.Consec_Pais, p.CodPais, p.Nombre, p.Imagen);
+
+            return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // DELETE: api/Consecutivos/5
-        public void Delete(int id)
+        // DELETE: api/Pais/5
+        [ResponseType(typeof(PaisModel))]
+        public IHttpActionResult DeletePais(int id)
         {
+            PaisModel p = CRUD.BuscarPaises().Where(e => e.PAISID == id).First();
+
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            PAIS.Eliminar(id);
+
+            return Ok(p);
+        }
+
+        private bool PaisExists(int id)
+        {
+            return CRUD.BuscarPaises().Count(e => e.PAISID == id) > 0;
         }
     }
 }
